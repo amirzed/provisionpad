1b7976dec3f55000dd3a6cdd1423f3b457b48da6
amirzed <amir.zainali@gmail.com>
2019-07-15 17:43:44 +0000

    add python pickle based db

---
+++ new/db/database.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pickle
+env_var_dir = os.environ['env_var_dir']
+sys.path.append(env_var_dir)
+database_name = os.path.join(env_var_dir, 'database.p')
+
+def initiate_db():
+    print('initiating the database')
+    database = {'created_instances': 0}
+    pickle.dump( database, open( database_name, 'wb' ) )
+    return database
+
+def load_database():
+    if os.path.isfile(database_name):
+        return pickle.load(open(database_name, 'rb'))
+    else: # used for initializing the database
+        return initiate_db
+
