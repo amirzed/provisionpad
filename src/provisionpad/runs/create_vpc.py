a3a1659d911a4a99dcf59e6925d5e5467003d3d0
amirzed <amir.zainali@gmail.com>
2019-07-22 23:25:13 +0000

    create vpc compatible with reading env_vars from json dump

--- src/provisionpad/runs/create_vpc.py
+++ src/provisionpad/runs/create_vpc.py
@@ -1,16 +1,16 @@
 import os
 import sys
 from provisionpad.aws.aws_ec2 import AWSec2Funcs
-from provisionpad.helpers.namehelpers import vpc_name
+# from provisionpad.helpers.namehelpers import vpc_name
 from provisionpad.db.database import load_database, save_database
 
-def create_vpc():
-    DB = load_database()
-    region = os.environ['aws_region']
-    access_key = os.environ['aws_access_key_id']
-    secret_key = os.environ['aws_secret_access_key']
+def create_vpc(env_vars, DB):
+    # DB = load_database()
+    region = env_vars['aws_region']
+    access_key = env_vars['access_key']
+    secret_key = env_vars['secret_key']
     awsf = AWSec2Funcs(region, access_key, secret_key)
-    thename = vpc_name()
+    thename = env_vars['vpc_name']
     if thename in DB:
         print ('The VPC with the name: {0} exists.'.format(thename))
         sys.exit()
@@ -22,5 +22,5 @@ def create_vpc():
     DB[thename]['subnet_id'] = vpc_params.subnet_id
     save_database(DB)
 
-if __name__ == "__main__":
-    create_vpc()
+# if __name__ == "__main__":
+#     create_vpc()
